95a46824fd8f6fa393708bc5a01d4ee5d210094c
Asikur <asikurrahman7@gmail.com>
2020-04-25 19:41:22 +0600

    Updates

---
+++ precache-manifest.81d8f8b522675a56ef6353f2d632dcd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54c6f9e00a99eef6ed19adc748f93edb",
+    "url": "/reactjs-monster/index.html"
+  },
+  {
+    "revision": "c9f1de08c6b8497df435",
+    "url": "/reactjs-monster/static/css/main.1914f049.chunk.css"
+  },
+  {
+    "revision": "f02cbc6836393a0ee16f",
+    "url": "/reactjs-monster/static/js/2.1e546497.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-monster/static/js/2.1e546497.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9f1de08c6b8497df435",
+    "url": "/reactjs-monster/static/js/main.50cf97ce.chunk.js"
+  },
+  {
+    "revision": "69ec5dc9d31d15d7af5f",
+    "url": "/reactjs-monster/static/js/runtime-main.9da01692.js"
+  }
+]);
